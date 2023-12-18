21fb5c1826b6eb9dbeb7f82d64f8059c1e762e5a
Rahul Kumar <kmrrhl09@gmail.com>
2023-12-18 07:12:22 +0530

    sass

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "./style.css";
+import styles from "./style.module.scss";
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button className={styles.root}>{props.label}</button>;
 };
 
 export default Button;
